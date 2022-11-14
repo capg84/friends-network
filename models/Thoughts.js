a7d15a516e1d019534f03bff3914144e82ec6338
capg84 <cyprian.gomes@live.co.uk>
2022-11-14 17:42:41 +0000

    updated thoughts schema to fix error

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,43 +1,7 @@
 const { Schema, model } = require("mongoose");
 const dayjs = require("dayjs");
 
-const thoughtsSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: true,
-      minLength: [1, "Oops, that's a very tiny thought."],
-      maxLength: [
-        280,
-        "You are thinking too much my friend, make this shorter.",
-      ],
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) =>
-        dayjs(createdAtVal).format("At hh:mm:ss on DD MMM YYYY"),
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    reactions: [reactionsSchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true,
-    },
-    id: false,
-  }
-);
-
-thoughtsSchema.virutal("reactionCount").get(function () {
-  return this.reactions.length;
-});
-
-//reactions schema
+//reactions schema (SCHEMA ONLY)
 const reactionsSchema = new Schema(
   {
     reactionId: {
@@ -72,6 +36,43 @@ const reactionsSchema = new Schema(
     id: false,
   }
 );
+
+//THOUGHTS schema
+const thoughtsSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: [1, "Oops, that's a very tiny thought."],
+      maxLength: [
+        280,
+        "You are thinking too much my friend, make this shorter.",
+      ],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        dayjs(createdAtVal).format("At hh:mm:ss on DD MMM YYYY"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionsSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtsSchema.virutal("reactionsCount").get(function () {
+  return this.reactions.length;
+});
 
 const Thoughts = model("Thoughts", thoughtsSchema);
 
